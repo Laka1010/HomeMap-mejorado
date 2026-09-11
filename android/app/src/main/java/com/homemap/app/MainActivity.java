cf6bb3f4b5392a34df13178e1cfc87e98238574b
Laka1010 <lucas.mesas.10@gmail.com>
2026-09-11 21:36:05 +0200

    Actualiza documentos legales

--- android/app/src/main/java/com/homemap/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.homemap.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- src/modules/index.js
+++
@@ -1,8 +0,0 @@
-export * from "./dashboard/DashboardOverview";
-export { default as Dashboard } from "./dashboard/Dashboard";
-export * from "./shopping/ShoppingModule";
-export * from "./shopping/ShoppingHistory";
-export * from "./tasks/TasksModule";
-export * from "./notes/NotesModule";
-export * from "./calendar/CalendarModule";
-export { EconomyModule } from "./economy/EconomyModule";
